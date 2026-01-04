6d30376c9a71a26d79e2dab920843f793ca13f92
si-rohit <rohit@viremail.com>
2026-01-04 13:43:47 +0530

    commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  getUsers,
+} = require("../controllers/userController");
+const protect = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/", protect, getUsers);
+
+module.exports = router;
